515c312f80c4adfeeae153f56fb36d1f854c6c18
jamiefang17 <jamiefang17@gmail.com>
2023-11-20 15:20:35 -0800

    initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello World!");
+        
+    
+    }
+}
